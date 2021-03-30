2806b64fc696e6f3e1ea6b9ce8b1febfb939a782
RichardR <richard.rountree@umajin.com>
2021-03-30 16:05:05 +1300

    Compile example 2 shaders.

--- Example 2/shader_uniforms.vert
+++ Example 2/shader_uniforms.vert
@@ -1,6 +1,6 @@
 #version 450
 #extension GL_GOOGLE_include_directive : enable
-#include "vulkan_common.glsl"
+#include "../vulkan_common.glsl"
 
 VERTEX_INPUT
 
--- compileShaders.bat
+++ compileShaders.bat
@@ -3,4 +3,10 @@
 
 fxc /nologo /O3 /T fx_5_0 /Fo "Example 1/custom.fxc" "Example 1/custom.fx"
 
+%VK_SDK_PATH%/Bin/glslangValidator.exe -V "Example 2/shader_uniforms.vert" -o "Example 2/shader_uniforms_vert.sprv"
+%VK_SDK_PATH%/Bin/glslangValidator.exe -V "Example 2/shader_uniforms.frag" -o "Example 2/shader_uniforms_frag.sprv"
+
+fxc /nologo /O3 /T fx_5_0 /Fo "Example 2/shader_uniforms.fxc" "Example 2/shader_uniforms.fx"
+
+
 pause
